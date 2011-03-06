514e260aef1a323e8d928f49b4de1154988531f5
Gabriel Andretta <ohhgabriel@gmail.com>
2011-03-06 14:15:04 -0300

    add docs to Storage::Filesystem

--- lib/adrift/storage.rb
+++ lib/adrift/storage.rb
@@ -1,8 +1,11 @@
 module Adrift
   module Storage
+    # Stores and removes files to and from the filesystem using
+    # queues.
     class Filesystem
       attr_reader :stored, :removed
 
+      # Creates a new Filesystem object.
       def initialize
         @queue_for_storage = []
         @queue_for_removal = []
@@ -10,14 +13,20 @@ module Adrift
         @removed = []
       end
 
+      # Indicates whether or not there are files that need to be
+      # stored or removed.
       def dirty?
         @queue_for_storage.any? || @queue_for_removal.any?
       end
 
+      # Adds the file +source_path+ to the storage queue, that will be
+      # saved in +destination_path+.  Note that in order to actually
+      # store the file you need to call #flush.
       def store(source_path, destination_path)
         @queue_for_storage << [source_path, destination_path]
       end
 
+      # Stores the files placed in the storage queue.
       def store!
         @queue_for_storage.each do |source_path, destination_path|
           FileUtils.mkdir_p(File.dirname(destination_path))
@@ -28,10 +37,13 @@ module Adrift
         @queue_for_storage.clear
       end
 
+      # Adds the file +path+ to the removal queue.  Note that in order
+      # to actually remove the file you need to call #flush.
       def remove(path)
         @queue_for_removal << path
       end
 
+      # Removes the files placed in the removal queue.
       def remove!
         @queue_for_removal.each do |path|
           FileUtils.rm(path) if File.exist?(path)
@@ -40,6 +52,8 @@ module Adrift
         @queue_for_removal.clear
       end
 
+      # Removes and then stores the files placed in the removal and
+      # storage queues, repectively.
       def flush
         remove!
         store!
