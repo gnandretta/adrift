8c1535dfeecc8e02bd1485e71c15b7b8dd848546
Gabriel Andretta <ohhgabriel@gmail.com>
2011-03-06 01:53:57 -0300

    remove attachment specs who doesn't spec behaviour

--- spec/adrift/attachment_spec.rb
+++ spec/adrift/attachment_spec.rb
@@ -173,50 +173,6 @@ module Adrift
   end
 
   describe Attachment do
-    describe ".default_options" do
-      let(:default_options) { Attachment.default_options }
-
-      it "has a default style" do
-        default_options.should have_key(:default_style)
-        default_options[:default_style].should == :original
-      end
-
-      it "doesn't have any styles defined" do
-        default_options.should have_key(:styles)
-        default_options[:styles].should == {}
-      end
-
-      it "has a default url for empty attachments" do
-        default_options.should have_key(:default_url)
-        default_options[:default_url].should == '/images/missing.png'
-      end
-
-      it "has a url pattern for non empty attachments" do
-        default_options.should have_key(:url)
-        default_options[:url].should == '/system/attachments/:class_name/:id/:attachment/:filename'
-      end
-
-      it "has a path pattern for non empty attachments" do
-        default_options.should have_key(:path)
-        default_options[:path].should == ':root/public:url'
-      end
-
-      it "has a class to build the attachment's storage" do
-        default_options.should have_key(:storage_class)
-        default_options[:storage_class].should == Storage::Filesystem
-      end
-
-      it "has a class to build the attachment's processor" do
-        default_options.should have_key(:processor_class)
-        default_options[:processor_class].should == Processor::Thumbnail
-      end
-
-      it "has a pattern class used to build patterns from strings" do
-        default_options.should have_key(:pattern_class)
-        default_options[:pattern_class].should == Pattern
-      end
-    end
-
     describe ".reset_default_options" do
       it "revert changes made to the default options" do
         original_value = Attachment.default_options[:url]
