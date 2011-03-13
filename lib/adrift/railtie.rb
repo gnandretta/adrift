48f3943efe080fddc234bc2c923bb46d4889418e
Gabriel Andretta <ohhgabriel@gmail.com>
2011-03-12 23:57:46 -0300

    fix railtie

--- lib/adrift/railtie.rb
+++ lib/adrift/railtie.rb
@@ -2,6 +2,11 @@ module Adrift
   class Railtie < Rails::Railtie
     initializer "adrift.setup" do
       Pattern::Tags::Root.path = Rails.root
+      ActiveSupport.on_load :active_record do
+        require 'adrift/integration/active_record'
+      end
+      # TODO find out a better way to go (?)
+      require 'adrift/integration/data_mapper'
     end
   end
 end
